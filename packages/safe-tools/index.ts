c5e963766d20f6ebc4781c094a26284b35abe2ec
Topper Bowers <topper@toppingdesign.com>
2023-05-15 20:11:40 +0200

    export addresses

--- packages/safe-tools/index.ts
+++ packages/safe-tools/index.ts
@@ -1,5 +1,8 @@
+import addresses from "addresses";
+
 export * from "./typechain-types";
 export * from "./src/SafeRelayer"
 export * from "./src/tokenCreator"
 export * from "./src/SafeSigner"
 export * from "./src/Multicaller"
+export * as addresses from "./src/addresses"
